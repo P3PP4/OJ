0e57894e7e535f2b36a0a0b7b07d1341563f78c9
YSL <109469241+P3PP4@users.noreply.github.com>
2023-03-27 08:32:46 +0900

    [Bronze I] Title: 약수, Time: 80 ms, Memory: 11580 KB -BaekjoonHub

--- 백준/Bronze/1037. 약수/약수.java
+++ 백준/Bronze/1037. 약수/약수.java
@@ -4,15 +4,13 @@ import java.util.StringTokenizer;
 
 public class Main {
 	
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringTokenizer st;
-	
 	public static void main(String[] args) throws Exception {
-	
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int num = Integer.parseInt(br.readLine());
 		int max = 0;
 		int min = Integer.MAX_VALUE;
-		st = new StringTokenizer(br.readLine(), " ");
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		for (int i = 0; i < num; i++) {
 			int input = Integer.parseInt(st.nextToken());
 			max = Math.max(max, input);
