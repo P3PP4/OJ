c57b16c53606aa2a0702762432fd8531eb6fec24
YSL <109469241+P3PP4@users.noreply.github.com>
2023-05-10 07:39:21 +0900

    [Silver IV] Title: 사이클 단어, Time: 108 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Silver/1544. 사이클 단어/사이클 단어.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+
+public class Main {
+	
+	public static void main(String[] args) throws Exception {
+	
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		HashSet<String> set = new HashSet<>();
+		int cnt = 0;
+		for (int i = 0; i < N; i++) {
+			int pre = set.size();
+			String input = br.readLine();
+			int len = input.length();
+			for (int j = 0; j < len; j++) {
+				StringBuilder sb = new StringBuilder();
+				for (int k = 0; k < len; k++) {
+					sb.append(input.charAt((j + k) % len));
+				}
+				if (set.contains(sb.toString())) break;
+				set.add(sb.toString());
+			}
+			if (pre < set.size()) cnt++;
+		}
+		
+		System.out.print(cnt);
+		
+	}
+	
+}
