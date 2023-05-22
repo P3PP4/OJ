ba95822d18fb0d356e841ebce05c16b890b4ade1
YSL <109469241+P3PP4@users.noreply.github.com>
2023-05-23 08:45:38 +0900

    [Silver III] Title: 숫자 야구, Time: 76 ms, Memory: 11624 KB -BaekjoonHub

--- 백준/Silver/2503. 숫자 야구/숫자 야구.java
+++ 백준/Silver/2503. 숫자 야구/숫자 야구.java
@@ -17,45 +17,43 @@ public class Main {
 			query[i][2] = Integer.parseInt(st.nextToken());
 		} // end of input
 		
-		LinkedList<Integer> list = new LinkedList<>();
+		int cnt = 0;
 		for (int i = 1; i < 10; i++) {
 			for (int j = 1; j < 10; j++) {
 				if (i == j) continue;
 				for (int k = 1; k < 10; k++) {
 					if (i == k || j == k) continue;
-					list.add(i * 100 + j * 10 + k);
+					
+					int n = i * 100 + j * 10 + k;
+					int[] a = new int[3];
+					int[] b = new int[3];
+					
+					a[0] = n / 100;
+					a[1] = n % 100 / 10;
+					a[2] = n % 10;
+					boolean flag = true;
+					
+					for (int l = 0; l < N; l++) {
+						b[0] = query[l][0] / 100;
+						b[1] = query[l][0] % 100 / 10;
+						b[2] = query[l][0] % 10;
+						int strike = 0;
+						int ball = 0;
+						
+						for (int m = 0; m < 3; m++) {
+							if (a[m] == b[m]) strike++;
+							else if (a[m] == b[(m + 1) % 3] || a[m] == b[(m + 2) % 3]) ball++;
+						}
+						
+						if (strike != query[l][1] || ball != query[l][2]) {
+							flag = false;
+							break;
+						}
+					}
+					
+					if (flag) cnt++;
 				}
 			}
-		}
-		
-		int[] a = new int[3];
-		int[] b = new int[3];
-		int cnt = 0;
-		for (Integer n : list) {
-			a[0] = n / 100;
-			a[1] = n % 100 / 10;
-			a[2] = n % 10;
-			boolean flag = true;
-			
-			for (int i = 0; i < N; i++) {
-				b[0] = query[i][0] / 100;
-				b[1] = query[i][0] % 100 / 10;
-				b[2] = query[i][0] % 10;
-				int strike = 0;
-				int ball = 0;
-				
-				for (int j = 0; j < 3; j++) {
-					if (a[j] == b[j]) strike++;
-					else if (a[j] == b[(j + 1) % 3] || a[j] == b[(j + 2) % 3]) ball++;
-				}
-				
-				if (strike != query[i][1] || ball != query[i][2]) {
-					flag = false;
-					break;
-				}
-			}
-			
-			if (flag) cnt++;
 		}
 		
 		System.out.print(cnt);
