9540c162e4b6bae6e470efc93678550fb0db92b3
YSL <109469241+P3PP4@users.noreply.github.com>
2023-05-24 16:54:23 +0900

    [Gold V] Title: 김밥천국의 계단, Time: 116 ms, Memory: 15976 KB -BaekjoonHub

--- 백준/Gold/28069. 김밥천국의 계단/김밥천국의 계단.java
+++ 백준/Gold/28069. 김밥천국의 계단/김밥천국의 계단.java
@@ -11,16 +11,12 @@ public class Main {
 		int N = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
 		int[] dp = new int[1000001];
-		for (int i = 1; i < 1000001; i++) {
-			dp[i] = 1234567;
-		}
-		
-		for (int i = 1; i < 1000001; i++) {
-			if (i < dp[i]) dp[i] = i;
-			if (dp[i - 1] + 1 < dp[i]) dp[i] = dp[i - 1] + 1;
+		dp[1] = 1;
+		for (int i = 2; i < 1000001; i++) {
+			if (dp[i] == 0) dp[i] = dp[i - 1] + 1;
 			
 			int next = i + (i / 2);
-			if (next <= 1000000) dp[next] = Math.min(dp[i] + 1, dp[next]);
+			if (next < 1000001) dp[next] = dp[i] + 1;
 		}
 		
 		System.out.print(dp[N] <= K ? "minigimbob" : "water");
