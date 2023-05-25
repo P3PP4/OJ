59658bc41d6821cc106fdeb22fd2c55022cce4f1
YSL <109469241+P3PP4@users.noreply.github.com>
2023-05-25 15:47:18 +0900

    [Silver III] Title: 회전하는 큐, Time: 76 ms, Memory: 11568 KB -BaekjoonHub

--- 백준/Silver/1021. 회전하는 큐/회전하는 큐.java
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Deque;
 import java.util.StringTokenizer;
 
@@ -12,24 +13,27 @@ public class Main {
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
-		Deque<Integer> dq = new ArrayDeque<>();
+		ArrayList<Integer> list = new ArrayList<>();
 		for (int i = 1; i <= N; i++) {
-			dq.add(i);
+			list.add(i);
 		}
 		int cnt = 0;
+		int index = 0;
+		int targetIndex;
 		int size = N;
 		st = new StringTokenizer(br.readLine(), " ");
 		for (int i = 0; i < M; i++) {
 			int target = Integer.parseInt(st.nextToken());
-			int temp = 0;
-			while (dq.peekFirst() != target) {
-				dq.addLast(dq.pollFirst());
-				temp++;
+			for (targetIndex = 0; targetIndex < N; targetIndex++) {
+				if (list.get(targetIndex) == target) break;
 			}
-			if (temp <= size >> 1) cnt += temp;
-			else cnt += size - temp;
-			dq.pollFirst();
+			int diff = Math.abs(targetIndex - index);
+			if (diff <= size >> 1) cnt += diff;
+			else cnt += size - diff;
+			list.remove(targetIndex);
 			size--;
+			index = targetIndex;
+			if (size <= index) index = 0;
 		}
 		
 		System.out.print(cnt);
