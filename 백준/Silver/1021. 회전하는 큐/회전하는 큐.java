a1a22d77f7001d7287acb99cf57324e7e4114f91
YSL <109469241+P3PP4@users.noreply.github.com>
2023-05-25 15:26:08 +0900

    [Silver III] Title: 회전하는 큐, Time: 76 ms, Memory: 11604 KB -BaekjoonHub

--- 백준/Silver/1021. 회전하는 큐/회전하는 큐.java
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.java
@@ -2,13 +2,9 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.List;
 import java.util.StringTokenizer;
-import java.util.stream.Collectors;
 
 public class Main {
-	
-	static Deque<Integer> dq = new ArrayDeque<>();
 	
 	public static void main(String[] args) throws Exception {
 	
@@ -16,6 +12,7 @@ public class Main {
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
+		Deque<Integer> dq = new ArrayDeque<>();
 		for (int i = 1; i <= N; i++) {
 			dq.add(i);
 		}
@@ -24,33 +21,19 @@ public class Main {
 		st = new StringTokenizer(br.readLine(), " ");
 		for (int i = 0; i < M; i++) {
 			int target = Integer.parseInt(st.nextToken());
-			int index = getIndex(size, target);
-			if (index <= size / 2) {
-				for (int j = 0; j < index; j++) {
-					dq.addLast(dq.pollFirst());
-				}
-				cnt += index;
-			} else {
-				for (int j = 0; j < size - index; j++) {
-					dq.addFirst(dq.pollLast());
-				}
-				cnt += size - index;
+			int temp = 0;
+			while (dq.peekFirst() != target) {
+				dq.addLast(dq.pollFirst());
+				temp++;
 			}
+			if (temp <= size >> 1) cnt += temp;
+			else cnt += size - temp;
 			dq.pollFirst();
-			--size;
+			size--;
 		}
 		
 		System.out.print(cnt);
 		
-	}
-	
-	static int getIndex(int size, int num) {
-		if (size < 2) return 0;
-		List<Integer> arr = dq.stream().collect(Collectors.toList());
-		for (int i = 0; i < size; i++) {
-			if (arr.get(i) == num) return i;
-		}
-		return 0;
 	}
 	
 }
