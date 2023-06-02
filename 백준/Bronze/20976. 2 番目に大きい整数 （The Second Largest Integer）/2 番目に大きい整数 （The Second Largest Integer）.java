1b357a42e311b9b9badddc1e3763af637767e950
YSL <109469241+P3PP4@users.noreply.github.com>
2023-06-03 08:35:53 +0900

    [Bronze IV] Title: 2 番目に大きい整数 (The Second Largest Integer), Time: 80 ms, Memory: 11560 KB -BaekjoonHub

---
+++ 백준/Bronze/20976. 2 番目に大きい整数 （The Second Largest Integer）/2 番目に大きい整数 （The Second Largest Integer）.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static void main(String[] args) throws Exception {
+	
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int[] input = new int[3];
+		for (int i = 0; i < 3; i++) {
+			input[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(input);
+		System.out.print(input[1]);
+	
+	}
+	
+}
