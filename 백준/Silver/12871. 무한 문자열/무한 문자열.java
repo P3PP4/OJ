f5978b984b9ac877b7579c5057a5afdaefbea455
YSL <109469241+P3PP4@users.noreply.github.com>
2023-06-07 09:08:20 +0900

    [Silver V] Title: 무한 문자열, Time: 80 ms, Memory: 12424 KB -BaekjoonHub

---
+++ 백준/Silver/12871. 무한 문자열/무한 문자열.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	
+	public static void main(String[] args) throws Exception {
+	
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String a = br.readLine();
+		String b = br.readLine();
+		String str1 = "";
+		String str2 = "";
+		for(int i = 0; i < b.length(); i++) {
+			str1 += a;
+		}
+		for(int i = 0; i < a.length(); i++) {
+			str2 += b;
+		}
+		if(str1.equals(str2)) System.out.print(1);
+		else System.out.print(0);
+		
+	}
+	
+}
