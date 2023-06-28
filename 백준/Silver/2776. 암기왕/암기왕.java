a642118485707217093c3e58b66e8e1eb4729595
YSL <109469241+P3PP4@users.noreply.github.com>
2023-06-29 08:45:31 +0900

    [Silver IV] Title: 암기왕, Time: 1752 ms, Memory: 384876 KB -BaekjoonHub

---
+++ 백준/Silver/2776. 암기왕/암기왕.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static void main(String[] args) throws Exception {
+	
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 0; tc < T; tc++) {
+			HashSet<Integer> set = new HashSet<>();
+			int N = Integer.parseInt(br.readLine());
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			for (int i = 0; i < N; i++) {
+				set.add(Integer.parseInt(st.nextToken()));
+			}
+			
+			int M = Integer.parseInt(br.readLine());
+			st = new StringTokenizer(br.readLine(), " ");
+			for (int i = 0; i < M; i++) {
+				if (set.contains(Integer.parseInt(st.nextToken()))) sb.append(1).append("\n");
+				else sb.append(0).append("\n");
+			}
+		}
+		
+		System.out.print(sb);
+	
+	}
+	
+}
