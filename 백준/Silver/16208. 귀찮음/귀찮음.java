554b7da5c6795d0110f976f509849f6922ae0098
YSL <109469241+P3PP4@users.noreply.github.com>
2023-07-03 09:50:31 +0900

    [Silver V] Title: 귀찮음, Time: 96 ms, Memory: 12820 KB -BaekjoonHub

---
+++ 백준/Silver/16208. 귀찮음/귀찮음.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static void main(String[] args) throws Exception {
+	
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] input = new int[N];
+		int len = 0;
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		for (int i = 0; i < N; i++) {
+			input[i] = Integer.parseInt(st.nextToken());
+			len += input[i];
+		}
+		Arrays.sort(input);
+		
+		long sum = 0;
+		for (int i = N - 1; i >= 0; i--) {
+			len -= input[i];
+			sum += len * input[i];
+		}
+		System.out.print(sum);
+	
+	}
+	
+}
