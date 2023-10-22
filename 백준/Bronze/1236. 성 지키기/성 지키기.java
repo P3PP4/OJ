dd51e6fbfd1c24573415f7601a40006b439ff645
YSL <109469241+P3PP4@users.noreply.github.com>
2023-10-22 11:42:02 +0900

    [Bronze I] Title: 성 지키기, Time: 76 ms, Memory: 11608 KB -BaekjoonHub

--- 백준/Bronze/1236. 성 지키기/성 지키기.java
+++ 백준/Bronze/1236. 성 지키기/성 지키기.java
@@ -1,36 +1,37 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-
-	public static void main(String[] args) throws IOException {
+	
+	public static void main(String[] args) throws Exception {
+	
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		char[][] arr = new char[n][m];
-		boolean[] row = new boolean[n];
-		boolean[] col = new boolean[m];
-		int r = 0;
-		int c = 0;
-		for(int i = 0; i < n; i++) {
-			arr[i] = br.readLine().toCharArray();
-			for(int j = 0; j < m; j++) {
-				if(arr[i][j] == 'X') {
-					row[i] = true;
-					col[j] = true;
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		char[][] map = new char[N][M];
+		boolean[] ver = new boolean[N];
+		boolean[] hor = new boolean[M];
+		int verCnt = 0;
+		int horCnt = 0;
+		for (int i = 0; i < N; i++) {
+			map[i] = br.readLine().toCharArray();
+			for (int j = 0; j < M; j++) {
+				if (map[i][j] == 'X') {
+					ver[i] = true;
+					hor[j] = true;
 				}
 			}
+			if (!ver[i]) verCnt++;
 		}
-		for(int i = 0; i < n; i++) {
-			if(!row[i]) r++;
+		
+		for (int i = 0; i < M; i++) {
+			if (!hor[i]) horCnt++;
 		}
-		for(int i = 0; i < m; i++) {
-			if(!col[i]) c++;
-		}
-		System.out.println(c > r ? c : r);
+		
+		System.out.print(verCnt < horCnt ? horCnt : verCnt);
+	
 	}
-
+	
 }
