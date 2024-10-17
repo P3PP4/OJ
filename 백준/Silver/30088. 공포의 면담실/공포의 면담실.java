2048b19ecb990b8c7f82ce73dc3aa1757f93d628
YSL <109469241+P3PP4@users.noreply.github.com>
2024-10-17 15:19:40 +0900

    [Silver V] Title: 공포의 면담실, Time: 76 ms, Memory: 11892 KB, Score: 36 point -BaekjoonHub

---
+++ 백준/Silver/30088. 공포의 면담실/공포의 면담실.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static void main(String[] args) throws Exception {
+	
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] sum = new int[N];
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			int employees = Integer.parseInt(st.nextToken());
+			for (int j = 0; j < employees; j++) {
+				sum[i] += Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		Arrays.sort(sum);
+		int[] add = new int[N];
+		int ans = add[0] = sum[0];
+		for (int i = 1; i < N; i++) {
+			add[i] = add[i - 1] + sum[i];
+			ans += add[i];
+		}
+		System.out.print(ans);
+		
+	}
+	
+}
