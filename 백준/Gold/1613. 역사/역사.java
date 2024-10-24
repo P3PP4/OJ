c4528c9efcd5812c4a239716ddd6e8d7b3fed3f4
YSL <109469241+P3PP4@users.noreply.github.com>
2024-10-24 20:43:47 +0900

    [Gold III] Title: 역사, Time: 472 ms, Memory: 36008 KB -BaekjoonHub

--- 백준/Gold/1613. 역사/역사.java
+++ 백준/Gold/1613. 역사/역사.java
@@ -1,6 +1,5 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -12,16 +11,12 @@ public class Main {
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		int N = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
-		int[][] dist = new int[N + 1][N + 1];
-		for (int i = 1; i <= N; i++) {
-			Arrays.fill(dist[i], 500);
-			dist[i][i] = 0;
-		}
+		boolean[][] dist = new boolean[N + 1][N + 1];
 		for (int i = 0; i < K; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
-			dist[a][b] = 1;
+			dist[a][b] = true;
 		}
 		
 		for (int i = 1; i <= N; i++) {
@@ -29,7 +24,7 @@ public class Main {
 				if (i == j) continue;
 				for (int k = 1; k <= N; k++) {
 					if (i == k || j == k) continue;
-					if (dist[j][k] > dist[j][i] + dist[i][k]) dist[j][k] = dist[j][i] + dist[i][k];
+					if (dist[j][i] && dist[i][k]) dist[j][k] = true;
 				}
 			}
 		}
@@ -39,8 +34,8 @@ public class Main {
 			st = new StringTokenizer(br.readLine(), " ");
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
-			if (dist[a][b] == 500) {
-				if (dist[b][a] == 500) sb.append(0);
+			if (!dist[a][b]) {
+				if (!dist[b][a]) sb.append(0);
 				else sb.append(1);
 			} else sb.append(-1);
 			sb.append("\n");
